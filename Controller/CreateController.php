e6fd764b5bc06f6e355d822e211182af3cb3fa99
Matthew Vanderende <matthew@vanderende.ca>
2021-01-20 14:36:32 -0500

    Implement new UserInterface

--- Controller/CreateController.php
+++ Controller/CreateController.php
@@ -7,7 +7,6 @@
 
 namespace bizbink\BlogBundle\Controller;
 
-use AppBundle\Entity\User;
 use bizbink\BlogBundle\Entity\Post;
 use bizbink\BlogBundle\Form\PostType;
 use Exception;
@@ -16,6 +15,7 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class CreateController extends AbstractController
 {
@@ -31,7 +31,7 @@ class CreateController extends AbstractController
     {
         $post = new Post();
         $author = $this->getUser();
-        if ($author instanceof User) {
+        if ($author instanceof UserInterface) {
             $post->setAuthor($author);
         }
 
